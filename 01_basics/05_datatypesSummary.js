eb87d4ede28a156c01d912a78c9266d9ca346a5e
jadon-vish <147917777+jadon-vish@users.noreply.github.com>
2024-01-23 07:56:19 +0000

    DatatypeSummary-memories

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -30,4 +30,26 @@ const myFunc=function(){
     console.log('Hello world');
 }
 
-console.log(typeof heroes);
+//console.log(typeof heroes);
+
+//STACK(Primitive)      HEAP(Non-primitive)
+
+let username1="Vishesh";
+
+let anotherUserName=username1;
+
+anotherUserName="Vishu"
+
+// console.log(username1);
+// console.log(anotherUserName); because copy pf username1 was given not the reference so change in value of anotherUserName will not affect username1
+
+let userOne={
+    email:"Vishesh@am.com",
+    upi:"123@ybl",
+}
+
+let userTwo=userOne;//reference of userOne will be given . so change in userTwo will affect the original value;
+
+userTwo.email="jadon@gmail.com";
+
+console.log(userOne.email);
